d7df83c00d1d1431842b23f947d84341c0659340
Rahul Shaw <67063884+alleverlvvr@users.noreply.github.com>
2021-12-09 19:36:12 +0530

    Added the whole chatbot file

---
+++ omicron-alvr-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\omicron-alvr-bot-83a0-Web-Deploy.pubxml -p:Password=rbyYCA8Mw5fcoWeiYegmXf6n6PcetaflNrnj6SkFrseE6PiMPLLKPCdA3uD0
+
